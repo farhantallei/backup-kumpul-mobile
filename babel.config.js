182261a6bdafd4225d2c18cbfa63bb2fa2fb2751
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2022-08-21 11:29:09 +0700

    Created a new Expo app

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@components': './src/components',
+            '@constants': './src/constants',
+            '@hooks': './src/hooks',
+            '@navigation': './src/navigation',
+            '@screens': './src/screens',
+            '@types': './src/types',
+          },
+        },
+      ],
+    ],
+  };
+};
